588db99461920c8ed7601341ebf4031252553759
Luke Kuzmish <lakuzmish@gmail.com>
2021-09-10 10:26:13 -0400

    fix default value

--- src/Traits/Jsonable.php
+++ src/Traits/Jsonable.php
@@ -4,7 +4,7 @@ namespace cosmastech\IntercomAppBuilder\Traits;
 
 trait Jsonable
 {
-    public function toJson($options)
+    public function toJson($options = 0)
     {
         return json_encode($this, $options);
     }
