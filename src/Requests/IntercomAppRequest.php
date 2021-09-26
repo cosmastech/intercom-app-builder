ffb7d0a09ebe454db6aaebe5356ff23aadb5a228
Luke Kuzmish <lakuzmish@gmail.com>
2021-09-26 19:49:22 -0400

    DTO for incoming Intercom app requests

---
+++ src/Requests/IntercomAppRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace cosmastech\IntercomAppBuilder\Requests;
+
+use Spatie\DataTransferObject\FlexibleDataTransferObject;
+
+class IntercomAppRequest extends FlexibleDataTransferObject
+{
+    public string $workspace_id;
+
+    public ?object $admin;
+
+    public ?object $user;
+
+    public ?object $conversation;
+
+    public ?object $card_creation_options;
+
+    public ?object $contact;
+
+    public ?object $context;
+
+    public ?string $component_id;
+
+    public ?object $current_canvas;
+
+    public ?object $input_values;
+}
